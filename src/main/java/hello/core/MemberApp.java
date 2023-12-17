accedc81d516516053b498a87dbaa73bbe5da393
dustj <qjsortls97@naver.com>
2023-12-17 23:45:15 +0900

    20231215 ioc,di그리고 컨테이너까지

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -7,11 +7,17 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
@@ -21,18 +27,4 @@ public class MemberApp {
 
     }
 
-    /*public static class OrderApp {
-
-        public static void main(String[] args) {
-            MemberService memberService = new MemberServiceImpl();
-            OrderService orderService = new OrderServiceImpl();
-
-            Long memberId=1L;
-            Member member = new Member(memberId, "memberA", Grade.VIP);
-            memberService.join(member);
-
-            Order order = orderService.createOrder(memberId, "itemA", 10000);
-
-        }
-    }*/
 }
